a4b2fa435a52daa4b4f8420466ce67f0c97f315e
Maverik Minett <maverik@maverik.io>
2025-09-16 09:24:12 -0400

    lint updates

--- src/lib/decorators/name.decorator.spec.ts
+++ src/lib/decorators/name.decorator.spec.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Name } from './name.decorator';
 import { MetadataDescriptor } from '../descriptors/metadata.descriptor';
 
