084dcb8f154d928e4ec3dea9cdb2f51448cc5ccc
Maverik Minett <maverik@maverik.io>
2025-09-16 09:23:13 -0400

    update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'agape-model',
+  displayName: 'agape-metadata',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/libs/model',
+  coverageDirectory: '../../../coverage/libs/metadata',
 };
